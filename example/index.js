907a002fc458f55edbb887a8e7e942e8023eb1fc
Ionică Bizău <bizauionica@gmail.com>
2016-10-23 13:19:00 +0300

    Initial working prototype

--- example/index.js
+++ example/index.js
@@ -3,3 +3,15 @@
 const debugMode = require("../lib");
 
 console.log(debugMode());
+// $ node example/
+// false
+
+// $ node debug example/
+// < Debugger listening on port 5858
+// debug> . ok
+// break in example/index.js:1
+// > 1 "use strict";
+//   2
+//   3 const debugMode = require("../lib");
+// debug> c
+// < true
